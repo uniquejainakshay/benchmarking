a2338d34bc6f0b6add7149d908f3c6535151a743
Shashank Khasare <shashank.khasare@gmail.com>
2015-01-27 19:09:30 +0530

    cpu scripts remove

--- disable_cpus.sh
+++
@@ -1,3 +0,0 @@
-no=`cat /proc/cpuinfo | grep processor | wc -l`
-no=`expr $no - 1`
-for (( c=1; c<=3; c++ )); do echo 0 | tee /sys/devices/system/cpu/cpu$c/online; done
--- enable_cpus.sh
+++
@@ -1,3 +0,0 @@
-no=`cat /proc/cpuinfo | grep processor | wc -l`
-no=`expr $no - 1`
-for (( c=1; c<=3; c++ )); do echo 1 | tee /sys/devices/system/cpu/cpu$c/online; done
